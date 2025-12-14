e4a5c8a29f769a66dd5c37b2436cec0bf4e6cf51
assembler-0 <diaviekone13@gmail.com>
2025-12-14 09:28:41 +0700

    cmake

--- cmake/source.cmake
+++ cmake/source.cmake
@@ -1,64 +1,14 @@
 # ============================================================================
 # Sources Organization
 # ============================================================================
-set(ASM_SOURCES
-    arch/x64/gdt_tss.asm
-    kernel/sched/switch.asm
-)
-
-set(ARCH_SOURCES
-    arch/x64/cpu.c
-    arch/x64/gdt/gdt.c
-    arch/x64/idt/idt.c
-    arch/x64/idt/isr.asm
-    arch/x64/idt/load_idt.asm
-    arch/x64/smp.c
-    arch/x64/tsc.c
-    arch/x64/irq/irq.c
-    arch/x64/exception.c
-    arch/x64/features/features.c
-)
-
-set(INIT_SOURCES
-    init/main.c
-)
-
-set(KERNEL_SOURCES
-    kernel/panic.c
-    kernel/sched/core.c
-    kernel/sched/fair.c
-    kernel/sched/process.c
-)
-
+file(GLOB_RECURSE ASM_SOURCES "arch/*.asm" "kernel/*.asm")
+file(GLOB_RECURSE ARCH_SOURCES "arch/*.c")
+file(GLOB_RECURSE INIT_SOURCES "init/*.c")
+file(GLOB_RECURSE KERNEL_SOURCES "kernel/*.c")
 file(GLOB_RECURSE DRIVER_SOURCES "drivers/*.c")
-
-set(LIB_SOURCES
-    lib/string.c
-    lib/printk.c
-    lib/log.c
-    lib/vsprintf.c
-    lib/ctype.c
-    lib/rbtree.c
-    lib/ringbuf.c
-    lib/bitmap.c
-)
-
-set(MM_SOURCES
-    mm/pmm.c
-    mm/vmm.c
-    mm/stack.c
-    mm/slab.c
-    mm/san/ubsan.c
-    mm/mmio.c
-    mm/vma.c
-    mm/vmalloc.c
-)
-
-set(CRYPTO_SOURCES
-    crypto/crc32.c
-    crypto/rng.c
-    crypto/sha/sha256.c
-)
+file(GLOB_RECURSE LIB_SOURCES "lib/*.c")
+file(GLOB_RECURSE MM_SOURCES "mm/*.c")
+file(GLOB_RECURSE CRYPTO_SOURCES "crypto/*.c")
 
 # ============================================================================
 # Build Include Directories
