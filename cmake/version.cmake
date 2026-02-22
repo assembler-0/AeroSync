de06a78b25799926c019e4d2c83dba87c78657f4
assembler-0 <diaviekone13@gmail.com>
2026-02-22 07:50:51 +0700

    bump ver

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -16,9 +16,9 @@ execute_process(
 )
 
 set(AEROSYNC_RELEASE "0" CACHE STRING "AeroSync release")
-set(AEROSYNC_CANDIDATE "3" CACHE STRING "AeroSync release candidate")
+set(AEROSYNC_CANDIDATE "4" CACHE STRING "AeroSync release candidate")
 set(AEROSYNC_ABI_LEVEL "0" CACHE STRING "AeroSync ABI level")
-set(AEROSYNC_CODENAME "Convergence" CACHE STRING "AeroSync codename")
+set(AEROSYNC_CODENAME "Integration" CACHE STRING "AeroSync codename")
 configure_file(
         ${CMAKE_CURRENT_SOURCE_DIR}/include/aerosync/version.h.in
         ${CMAKE_CURRENT_SOURCE_DIR}/include/aerosync/version.h
